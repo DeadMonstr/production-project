97c6006ea7184fb821870c363b6a2252edfe26be
DeadMonstr <deedmonstr@gmail.com>
2024-01-04 19:41:15 +0500

    changed sidebar and navbar, karoche chiroyli boldi

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button, ThemeButton } from './Button'
+import {Button, ButtonSize, ButtonTheme} from './Button'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
 
@@ -27,7 +27,7 @@ Primary.decorators = [ThemeDecorator(Theme.LIGHT)]
 export const SecondaryLight: Story = {
     args: {
         children: 'Button',
-        theme: ThemeButton.CLEAR
+        theme: ButtonTheme.CLEAR
     }
 }
 SecondaryLight.decorators = [ThemeDecorator(Theme.LIGHT)]
@@ -35,7 +35,7 @@ SecondaryLight.decorators = [ThemeDecorator(Theme.LIGHT)]
 export const SecondaryDark: Story = {
     args: {
         children: 'Button',
-        theme: ThemeButton.CLEAR
+        theme: ButtonTheme.CLEAR
     }
 }
 SecondaryDark.decorators = [ThemeDecorator(Theme.DARK)]
@@ -43,7 +43,7 @@ SecondaryDark.decorators = [ThemeDecorator(Theme.DARK)]
 export const OutlineLight: Story = {
     args: {
         children: 'Button',
-        theme: ThemeButton.OUTLINE
+        theme: ButtonTheme.OUTLINE
     }
 }
 
@@ -52,8 +52,70 @@ OutlineLight.decorators = [ThemeDecorator(Theme.LIGHT)]
 export const OutlineDark: Story = {
     args: {
         children: 'Button',
-        theme: ThemeButton.OUTLINE
+        theme: ButtonTheme.OUTLINE
     }
 }
 
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)]
+
+
+export const Background: Story = {
+    args: {
+        children: 'Button',
+        theme: ButtonTheme.BACKGROUND
+    }
+}
+
+
+
+
+export const BackgroundInverted: Story = {
+    args: {
+        children: 'Button',
+        theme: ButtonTheme.BACKGROUND_INVERTED
+    }
+}
+
+export const Square: Story = {
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND,
+        square: true
+    }
+}
+
+
+export const SquareSizeM: Story = {
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+        size: ButtonSize.M
+    }
+}
+
+SquareSizeM.decorators = [ThemeDecorator(Theme.DARK)]
+
+export const SquareSizeL: Story = {
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+        size: ButtonSize.L
+    }
+}
+
+SquareSizeL.decorators = [ThemeDecorator(Theme.DARK)]
+
+export const SquareSizeXl: Story = {
+    args: {
+        children: '>',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+        square: true,
+        size: ButtonSize.XL
+    }
+}
+
+SquareSizeXl.decorators = [ThemeDecorator(Theme.DARK)]
+
+// BackgroundInverted.decorators = [ThemeDecorator(Theme.LIGHT)]
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,17 +1,22 @@
-import { classNames } from 'shared/lib/classNames/classNames'
+import {classNames} from 'shared/lib/classNames/classNames'
 import cls from './Sidebar.module.scss'
-import { useState } from 'react'
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
-import { Button } from 'shared/ui/Button/Button'
-import { useTranslation } from 'react-i18next'
+import {useState} from 'react'
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher'
+import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher'
+import {Button, ButtonSize, ButtonTheme} from 'shared/ui/Button/Button'
+import {useTranslation} from 'react-i18next'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {RoutePath} from "shared/config/routeConfig/routeConfig";
+
+import AboutIcon from "shared/assets/icons/about.svg"
+import MainIcon from "shared/assets/icons/home.svg"
 
 interface SidebarProps {
     className?: string
 }
 
-export const Sidebar = ({ className }: SidebarProps) => {
-    const { t } = useTranslation()
+export const Sidebar = ({className}: SidebarProps) => {
+    const {t} = useTranslation()
     const [collapsed, setCollapsed] = useState(false)
 
     const onToggle = (): void => {
@@ -21,15 +26,54 @@ export const Sidebar = ({ className }: SidebarProps) => {
     return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-            <Button data-testid="sidebar-toggle" onClick={onToggle}>
-                {t('toggle')}
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+                className={cls.collapsedBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                square
+                size={ButtonSize.XL}
+            >
+                {collapsed ? ">" : "<"}
             </Button>
+            <div className={cls.items}>
+
+
+                <AppLink
+                    className={cls.item}
+                    theme={AppLinkTheme.PRIMARY}
+                    to={RoutePath.main}
+
+                >
+                    <MainIcon className={cls.icon}/>
+                    <span
+                        className={cls.link}
+                    >
+                        {t('mainPage')}
+                    </span>
+
+                </AppLink>
+
+
+                <AppLink
+                    theme={AppLinkTheme.PRIMARY}
+                    to={RoutePath.about}
+                    className={cls.item}
+                >
+                    <AboutIcon className={cls.icon}/>
+                    <span className={cls.link}>
+                        {t('aboutPage')}
+                    </span>
+                </AppLink>
+
+
+            </div>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher/>
-                <LangSwitcher className={cls.lang}/>
+                <LangSwitcher short className={cls.lang}/>
             </div>
         </div>
     )
