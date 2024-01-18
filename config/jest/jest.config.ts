0e11688eddf885b6e5aaa6349d1de6e55f35f8f1
DeadMonstr <deedmonstr@gmail.com>
2024-01-18 20:59:20 +0500

    added react-redux and toolkit, added Counter entity and test to new funcs

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -43,7 +43,10 @@ const config: Config = {
     testMatch: [
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'
     ],
-    rootDir: '../../'
+    rootDir: '../../',
+    globals: {
+        __IS_DEV__ : true
+    },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
